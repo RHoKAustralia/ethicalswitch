f2f83fe240f9543a7fabb8438a23d81a1ca20b69
Sergei Matheson <sergei.matheson@gmail.com>
2014-05-31 14:16:34 +1000

    Added routing skeleton

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,29 @@
 'use strict';
 
 angular
-  .module('ethicalSwitchApp', []);
+  .module('ethicalSwitchApp', ['facebook', 'ui.router'])
+  .config(['FacebookProvider', function(FacebookProvider) {
+    // Here you could set your appId through the setAppId method and then initialize
+    // or use the shortcut in the initialize method directly.
+    FacebookProvider.init('my-ap-id');
+  }])
+  .config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+    .state('app', {
+      //controller: 'AppCtrl',
+      templateUrl: 'views/app.html',
+      abstract: true,
+    })
+    .state('app.main', {
+      url: '/',
+      //controller: 'MainCtrl',
+      templateUrl: 'views/main.html'
+    })
+    .state('app.faq', {
+      url: '/faq',
+      //controller: 'FaqCtrl',
+      templateUrl: 'views/faq.html'
+    });
+  });
+
